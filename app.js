ca03a1e134c7942c68d142c096e3b33c6a9c3313
DaCode-Cyber <109779574+DaCode-Cyber@users.noreply.github.com>
2023-09-12 12:24:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,49 +2,46 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+// app.use(express.json())
+
 app.get('/api', (req, res) => {
-  const slackName = req.query.slack_name;
-  const track = req.query.track;
+  try {
+    const { slack_name, track } = req.query;
 
-  // Get current day of the week
-  const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
+    const currentDate = new Date();
+    const year = currentDate.getUTCFullYear();
+    const month = (currentDate.getUTCMonth() + 1).toString().padStart(2, "0");
+    const day = currentDate.getUTCDate().toString().padStart(2, "0");
+    const hours = currentDate.getUTCHours().toString().padStart(2, "0");
+    const Minutes = currentDate.getUTCMinutes().toString().padStart(2, "0");
+    const seconds = currentDate.getUTCSeconds().toString().padStart(2, "0");
 
-  // Get current UTC time
-  const currentUtcTime = new Date().toISOString();
+    // create the formatted UTC time string
 
-  // Validate UTC time within +/-2 hours
-  const currentTime = new Date();
-  const twoHoursAgo = new Date(currentTime);
-  twoHoursAgo.setHours(currentTime.getHours() - 2);
-  const twoHoursLater = new Date(currentTime);
-  twoHoursLater.setHours(currentTime.getHours() + 2);
+    const currentUTC = '${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z';
 
-  const isWithinTwoHours = currentTime >= twoHoursAgo && currentTime <= twoHoursLater;
-
-  // GitHub URLs
-  const githubUrlFile = 'https://github.com/DaCode-Cyber/Date-Api/blob/main/app.js';
-  const githubUrlSource = 'https://github.com/DaCode-Cyber/Date-Api';
-
-  // Response JSON
-  const response = {
-    slack_name: "Da_Code",
-    current_day: currentDay,
-    utc_time: currentUtcTime,
-    track: "backend",
-    github_file_url: githubUrlFile,
-    github_repo_url: githubUrlSource,
-    status_code: '200'
-  };
-
-  // Add time validation status
-  if (isWithinTwoHours) {
-    response.time_validation = 'Within +/-2 hours';
-  } else {
-    response.time_validation = 'Outside +/-2 hours';
-  }
-
-  res.json(response);
+    const response = {
+     slack_name,
+     current_day: toLocaleDateString('en-US', { weekday: 'long' }),
+     utc_time: currentUTC,
+     track,
+     github_file_url: 'https://github.com/DaCode-Cyber/Date-Api/blob/main/app.js',
+     github_repo_url: 'https://github.com/DaCode-Cyber/Date-Api',
+     status_code: 200,
+     message: "successful"    
+    };
+   res.json(response);
+   } catch(error) {
+    console.error('An error occurred:', error || error.message);
+    res.status(500).json({error: 'Internal Server Error' || error.message});
+   }
 });
+
+app.get('/', (req, res)=> {
+ res.json({
+  message: 'homepage'
+ })
+})
 
 app.listen(PORT, () => {
  console.log('server started on port ${PORT}');
