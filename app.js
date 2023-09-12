a1ef3d0875ffbf8227edde8f3ef1b69ca5a1cea9
DaCode-Cyber <109779574+DaCode-Cyber@users.noreply.github.com>
2023-09-12 12:31:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+
+require('dotenv').config()
+app.use(express.json())
+
 const PORT = process.env.PORT || 3030;
 
-// app.use(express.json())
+app.use(express.json())
 
 app.get('/api', (req, res) => {
   try {
